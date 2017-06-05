25c93c75c64866eb9eb5828dad824c463374337b
Strumwulf <Strumwulf@users.noreply.github.com>
2017-06-05 14:18:00 -0700

    Update train_audio.py

--- train_audio.py
+++ train_audio.py
@@ -8,6 +8,6 @@ recog.speaker_name = 'David'
 recog.debug = True
 
 # train model with the newly recorded file
-recog.train_new_data('/home/pi/Desktop/Omega2.0/training_audio/david5.wav')
+recog.train_new_data('entry.wav')
 
 print recog.get_speakers()
